48026631d415c1f9a43054ec5c77a1c0429ff770
FR-Marie <marie.favre.rochex@gmail.com>
2022-03-22 15:35:19 +0100

    ajouter formateurs ok

--- supprimer_formateur.php
+++ supprimer_formateur.php
@@ -23,7 +23,7 @@ if (isset($_POST["btn-deconnexion"])){
     <!-----bootstrap---->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Détails formateur</title>
+    <title>Supprimer formateur</title>
 
 
     <!-- Favicon-->
@@ -167,6 +167,7 @@ if($db){
                 ?>
                 <div class="alert alert-success w-50 m-auto text-center">Suppression réussie</div>
                 <div class="text-center">
+                    <a href="affichage_formateurs.php" class="btn btn-outline-dark">Voir formateurs</a>
                     <a href="accueil.php" class="btn btn-secondary w-25 m-auto text-center mt-5 mb-5">Retour accueil</a>
                 </div>
                 <style>
