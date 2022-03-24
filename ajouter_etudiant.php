363748fd70197057f02826fe43013c4072730888
assiaabbas <assia.abbas.zidane@gmail.com>
2022-03-24 10:04:27 +0100

    ajout nav et fouter

--- ajouter_etudiant.php
+++ ajouter_etudiant.php
@@ -35,23 +35,22 @@ if(isset($_SESSION["email"])){
 
 
 
-    <!-- Responsive navbar-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container px-5">
-            <a class="navbar-brand" href="#!">LYCEE HENRI-IV</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="accueil.php">Accueil</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">Connexion</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">Services</a></li>
-                </ul>
-            </div>
+<!-- Responsive navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark p-4">
+    <div class="container px-5">
+        <a class="navbar-brand" href="#!">☼ LYCEE HENRI-IV ☼</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="accueil.php">Accueil</a></li>
+                <li class="nav-item"><a class="nav-link" href="affichage_admins.php">Admins</a></li>
+                <li class="nav-item"><a class="nav-link" href="affichage_formateurs.php">Formateurs</a></li>
+                <li class="nav-item"><a class="nav-link" href="eleves.php">Eleves</a></li>
+                <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+            </ul>
         </div>
-    </nav>
-
-
+    </div>
+</nav>
 <!------------------------------------------------->
 
     <?php
@@ -286,11 +285,25 @@ if(isset($_SESSION["email"])){
 
     ?>
 
-    <!-- Footer-->
-    <footer class="py-5 bg-dark">
-        <div class="container px-4 px-lg-5"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
-
-    </footer>
+   <!-- Footer-->
+<footer class="py-5 bg-dark">
+    <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container px-5">
+            <a class="navbar-brand" href="#!">☼ LYCEE HENRI-IV ☼</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="accueil.php">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="affichage_admins.php">Admins</a></li>
+                    <li class="nav-item"><a class="nav-link" href="affichage_formateurs.php">Formateurs</a></li>
+                    <li class="nav-item"><a class="nav-link" href="eleves.php">Eleves</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container px-4 px-lg-5 mt-4"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+</footer>
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
--- editer_eleves.php
+++ editer_eleves.php
@@ -36,21 +36,21 @@ if (isset($_POST["btn-deconnexion"])){
 
 
 <!-- Responsive navbar-->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark p-4">
     <div class="container px-5">
-        <a class="navbar-brand" href="#!">Lycée Henri IV</a>
+        <a class="navbar-brand" href="#!">☼ LYCEE HENRI-IV ☼</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="accueil.php">Accueil</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">Connexion</a></li>
+                <li class="nav-item"><a class="nav-link" href="affichage_admins.php">Admins</a></li>
+                <li class="nav-item"><a class="nav-link" href="affichage_formateurs.php">Formateurs</a></li>
+                <li class="nav-item"><a class="nav-link" href="eleves.php">Eleves</a></li>
                 <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">Services</a></li>
             </ul>
         </div>
     </div>
 </nav>
-
 
 <!-------------------------SQL POUR INFOS PERSONNELLES------------------------>
 
@@ -289,14 +289,24 @@ if($db){
 
 
 ?>
-</div>
-<!-- Footer-->
+</div><!-- Footer-->
 <footer class="py-5 bg-dark">
-
-    <div class="container px-4 px-lg-5 w-100"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
-
-    
-
+    <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container px-5">
+            <a class="navbar-brand" href="#!">☼ LYCEE HENRI-IV ☼</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="accueil.php">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="affichage_admins.php">Admins</a></li>
+                    <li class="nav-item"><a class="nav-link" href="affichage_formateurs.php">Formateurs</a></li>
+                    <li class="nav-item"><a class="nav-link" href="eleves.php">Eleves</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container px-4 px-lg-5 mt-4"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
 </footer>
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
